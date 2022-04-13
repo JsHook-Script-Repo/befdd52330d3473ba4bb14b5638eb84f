058c97c0968a2ec529599ebd3a0e7d9a9b9f5460
xlazl <67002539+xlazl@users.noreply.github.com>
2022-04-14 01:52:26 +0800

    Add files via upload

---
+++ rhino.js
@@ -0,0 +1,12 @@
+common.hookAllMethods('com.cjtec.remoteassis.bean.UserTime', 'getRemaining_time', function (param) {
+    param.setResult('9999999');
+});
+common.hookAllMethods('com.cjtec.remoteassis.App','p',function(param){
+param.setResult(true);
+});
+common.hookAllMethods('com.cjtec.remoteassis.bean.UserInfo','getVipendtime',function(param){
+param.setResult('永不过期啦');
+});
+common.hookAllMethods('com.cjtec.remoteassis.App','q',function(param){
+param.setResult(true);
+});
